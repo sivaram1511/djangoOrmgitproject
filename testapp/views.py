f79b7a2acb8eed0988d0e593fb531f040d6d8bdf
sivaram <sivaramk1411@gmail.com>
2021-05-29 13:21:50 +0530

    in this query using if a particuler object is available return all that name of object

--- testapp/views.py
+++ testapp/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from testapp.models import Employee
 # Create your views here.
 def display_view(request):
-    emp=Employee.objects.filter(esal__gt=50000)
-
+    emp=Employee.objects.filter(ename__icontains='SU')
     return render(request,'testapp/index.html',{"employee":emp})
