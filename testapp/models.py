fd6a93fcd3eb4b33428c9faee931059b747c4f18
sivaram <sivaramk1411@gmail.com>
2021-05-29 11:19:59 +0530

    create model,modeladmin classes for create table in database

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+class Employee(models.Model):
+    eno=models.IntegerField()
+    ename=models.CharField(max_length=65)
+    esal=models.FloatField()
+    eaddr=models.CharField(max_length=256)
 # Create your models here.
