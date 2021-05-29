88f60c92a04a4dd5276f38fb3c383fd0595f5dfa
sivaram <sivaramk1411@gmail.com>
2021-05-29 11:33:36 +0530

    create base.html file

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from testapp.models import Employee
 # Create your views here.
+def display_view(request):
+    employee=Employee.objects.all()
+    return render(request,'testapp/index.html',{"employee":employee})
