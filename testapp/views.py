c7fee5c2899a45b83fe4350cae58582c280c1d33
sivaram <sivaramk1411@gmail.com>
2021-06-02 10:02:30 +0530

    using aggregate min function for minimum salary

--- testapp/views.py
+++ testapp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from testapp.models import Employee
-from django.db.models import Q,Avg
+from django.db.models import Q,Avg,Min
 from django.db.models.functions import Lower
 # Create your views here.
 def display_view(request):
-    employee=Employee.objects.all().aggregate(Avg('esal'))
+    employee=Employee.objects.all().order_by('esal')[0:1]
 
     return render(request,'testapp/index.html',{"employee":employee})
 def aggregate_view(request):
-    avgsal=Employee.objects.all().aggregate(Avg('esal'))
-    return render(request,'testapp/index.html',{"avg":avgsal})
+   minsal=Employee.objects.all().aggregate(Min('esal'))
+   return render(request,'testapp/index.html',{"minsal":minsal})
