9cf8ff732972ea40c4f1daaaecb16da55fa59e78
sivaram <sivaramk1411@gmail.com>
2021-06-02 10:13:33 +0530

    i have writen the queries for 2nd highest eno and max eno using views functions

--- testapp/views.py
+++ testapp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from testapp.models import Employee
-from django.db.models import Q,Avg,Min
+from django.db.models import Q,Avg,Min,Max
 from django.db.models.functions import Lower
 # Create your views here.
 def display_view(request):
-    employee=Employee.objects.all().order_by('esal')[0:1]
+    employee=Employee.objects.all().order_by('-eno')[1:2]
 
     return render(request,'testapp/index.html',{"employee":employee})
 def aggregate_view(request):
-   minsal=Employee.objects.all().aggregate(Min('esal'))
-   return render(request,'testapp/index.html',{"minsal":minsal})
+   maxeno=Employee.objects.all().aggregate(Max('eno'))
+   return render(request,'testapp/index.html',{"minsal":maxeno})
