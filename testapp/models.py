22da0fde8dc50d711cb86f3dd268f15b99b9ffdf
sivaram <sivaramk1411@gmail.com>
2021-06-07 17:18:25 +0530

    range and sorted the esal in models class

--- testapp/models.py
+++ testapp/models.py
@@ -2,6 +2,10 @@ from django.db import models
 class CustomManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().order_by('eno')
+    def get_emp_sal_range(self,esal1,esal2):
+
+        return super().get_queryset().filter(esal__range=(esal1,esal2)).order_by('esal')
+    #def get_emp_
 class Employee(models.Model):
 
     eno=models.IntegerField()
--- testapp/views.py
+++ testapp/views.py
@@ -4,8 +4,8 @@ from django.db.models import Q,Avg,Min,Max,Sum
 from django.db.models.functions import Lower
 # Create your views here.
 def display_view(request):
-    employee=Employee.objects.all()
-
+    #employee=Employee.objects.all()
+    employee=Employee.objects.get_emp_sal_range(12000,30000)
     return render(request,'testapp/index.html',{"employee":employee})
 def aggregate_view(request):
    sumofesal=Employee.objects.count()
