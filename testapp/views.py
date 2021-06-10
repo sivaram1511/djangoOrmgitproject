afd034104a3929fb466c41c3c406e93b234cfeaf
sivaram <sivaramk1411@gmail.com>
2021-06-10 11:05:32 +0530

    get the queryset objects used by Values_list() it retruns tuple

--- testapp/views.py
+++ testapp/views.py
@@ -5,8 +5,10 @@ from django.db.models.functions import Lower
 # Create your views here.
 def display_view(request):
     #employee=Employee.objects.all()
-    employee=ProxyEmployee.objects.values('ename',ename__contains='sa')
-    return render(request,'testapp/index.html',{"employee":employee})
+    '''employee=ProxyEmployee.objects.values('ename',ename__contains='sa')
+    return render(request,'testapp/index.html',{"employee":employee})'''
+    emp=Employee.objects.values_list("eno","ename")
+    return render(request,'testapp/index.html',{"emp":emp})
 def aggregate_view(request):
    sumofesal=Employee.objects.count()
    return render(request,'testapp/index.html',{"sumsal":sumofesal})
