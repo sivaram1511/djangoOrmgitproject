100925a75e146417028dd12937906f6e2fe68c13
sivaram <sivaramk1411@gmail.com>
2021-06-15 17:24:06 +0530

    create method is not working i dont know wt is reason saying iterator error

--- testapp/views.py
+++ testapp/views.py
@@ -7,8 +7,16 @@ def display_view(request):
     #employee=Employee.objects.all()
     '''employee=ProxyEmployee.objects.values('ename',ename__contains='sa')
     return render(request,'testapp/index.html',{"employee":employee})'''
-    emp=Employee.objects.values_list("eno",flat=True).order_by('eno')
-    return render(request,'testapp/index.html',{"emp":emp})
+    #employee=Employee.objects.
+    e=Employee()
+    e.eno=127
+    e.ename='kagithala'
+    e.esal=90890
+    e.eaddr="Tuni"
+    employee=Employee.objects.create(e)
+    for i in employee:
+        print(i)
+    return render(request,'testapp/index.html',{"employee":employee})
 def aggregate_view(request):
    sumofesal=Employee.objects.count()
    return render(request,'testapp/index.html',{"sumsal":sumofesal})
