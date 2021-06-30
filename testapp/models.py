6450c3c845f19a2f6a586beac0133893c5bab7c0
sivaram <sivaramk1411@gmail.com>
2021-06-30 23:00:44 +0530

    django orm interviews query assignments

--- testapp/models.py
+++ testapp/models.py
@@ -1,18 +1,11 @@
 from django.db import models
+
 class CustomManager(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().order_by('eno')
-    def get_emp_sal_range(self,esal1,esal2):
+   ''''' def get_queryset(self):
 
-        return super().get_queryset().filter(esal__range=(esal1,esal2)).order_by('esal')
-    def get_emp_order_by(self,param):
-        return super().get_queryset().order_by(param)
-class  CustomManager2(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().order_by('ename')
-    def get_emp_sal_not_ename(self):
-        return super().get_queryset().exclude(ename__startswith='b')
-
+         return super().get_queryset().filter(esal__gt=19000).order_by("esal").filter(ename__startswith='b')'''
+   def get_emp_sal_range(self,esal1,esal2):
+        return super().get_queryset().filter(esal__range=(esal1,esal2)).order_by('-esal')
 class Employee(models.Model):
 
     eno=models.IntegerField()
@@ -23,6 +16,6 @@ class Employee(models.Model):
     objects=CustomManager()
 # Create your models here.
 class ProxyEmployee(Employee):
-    objects = CustomManager2()
+    #objects = CustomManager2()
     class Meta:
         proxy=True
--- testapp/views.py
+++ testapp/views.py
@@ -4,18 +4,15 @@ from django.db.models import Q,Avg,Min,Max,Sum
 from django.db.models.functions import Lower
 # Create your views here.
 def display_view(request):
-    #employee=Employee.objects.all()
-    '''employee=ProxyEmployee.objects.values('ename',ename__contains='sa')
-    return render(request,'testapp/index.html',{"employee":employee})'''
-    #employee=Employee.objects.
-    e=Employee()
-    e.eno=127
-    e.ename='kagithala'
-    e.esal=90890
-    e.eaddr="Tuni"
-    employee=Employee.objects.create(e)
-    for i in employee:
-        print(i)
+    #employee=Employee.objects.all().aggregate(Avg('esal'))
+    #employee=Employee.objects.all().order_by('-esal')[:1]
+    #employee=Employee.objects.order_by('-esal')[:2]
+    #employee=Employee.objects.all().filter(esal__lt=25000)
+    #q1=Employee.objects.all().aggregate(Avg('esal'))
+    employee=Employee.objects.exclude(id__range=[0,10])
+
+
+
     return render(request,'testapp/index.html',{"employee":employee})
 def aggregate_view(request):
    sumofesal=Employee.objects.count()
