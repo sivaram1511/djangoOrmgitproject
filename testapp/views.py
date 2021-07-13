33ad74b515ebe8540aebde298b6a5a4fa2ddacbe
sivaram <sivaramk1411@gmail.com>
2021-07-13 22:43:28 +0530

    we can get specified fields using values

--- testapp/views.py
+++ testapp/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render
 from testapp.models import Employee,ProxyEmployee
-from django.db.models import Q,Avg,Min,Max,Sum
+from django.db.models import Q,Avg,Min,Max,Sum,F
 from django.db.models.functions import Lower
 # Create your views here.
 def display_view(request):
-    #employee=Employee.objects.all().aggregate(Avg('esal'))
-    #employee=Employee.objects.all().order_by('-esal')[:1]
-    #employee=Employee.objects.order_by('-esal')[:2]
-    #employee=Employee.objects.all().filter(esal__lt=25000)
-    #q1=Employee.objects.all().aggregate(Avg('esal'))
-    employee=Employee.objects.exclude(id__range=[0,10])
+    #qs=Employee.objects.filter(~Q(eno__lt=100))
+    qs=Employee.objects.filter(ename__startswith='r').values('ename','esal')
+
+    print(qs.query)
 
 
 
-    return render(request,'testapp/index.html',{"employee":employee})
+    return render(request,'testapp/index.html',{'employee':qs})
 def aggregate_view(request):
    sumofesal=Employee.objects.count()
    return render(request,'testapp/index.html',{"sumsal":sumofesal})
