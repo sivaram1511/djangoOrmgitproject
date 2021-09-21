04cb8b36111ce2ef105f7322ebdb27f2683aea7d
sivaram1511 <sivaramk1411@gmail.com>
2021-09-21 08:09:33 +0530

    case insensitive iexacet command

--- testapp/views.py
+++ testapp/views.py
@@ -5,13 +5,20 @@ from django.db.models.functions import Lower
 # Create your views here.
 def display_view(request):
     #qs=Employee.objects.filter(~Q(eno__lt=100))
-    qs=Employee.objects.filter(ename__startswith='r').values('ename','esal')
-
-    print(qs.query)
+    #qs=Employee.objects.all().aggregate(Max('esal'))
+    #qs=Employee.objects.exclude(esal=99870).order_by("-esal")
+    #a=set()
+    #qs=Employee.objects.order_by('-esal')
+    #for i in qs:
+       # a.add(i.esal)
+    #c=sorted(a)
+    #print(c)
+    #xy=Employee.objects.filter(esal=c[-2])
+    qs=Employee.objects.filter(ename__iexact="TOm")
 
 
 
     return render(request,'testapp/index.html',{'employee':qs})
 def aggregate_view(request):
-   sumofesal=Employee.objects.count()
-   return render(request,'testapp/index.html',{"sumsal":sumofesal})
+   qs=Employee.objects.all().exclude(esal=5000)
+   return render(request,'testapp/index.html',{"maxsal":qs})
